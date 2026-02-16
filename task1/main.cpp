1c65ad46950dc29ed7ca3b678f20e00b3cec9df8
KrutalevickLev <krutalevichlev@gmail.com>
2026-02-16 17:42:11 +0300

    Add project files.

---
+++ task1/main.cpp
@@ -0,0 +1,24 @@
+//#include <iostream> 
+//#define STUDENDS_NUMBER 3.0
+//
+//using namespace std;
+//
+//
+//double calculate_average_score(int a, int b, int c);
+//
+//int main(){
+//	int mark1, mark2, mark3;
+//
+//	cout << "Enter 3 marks: ";
+//	cin >> mark1 >> mark2 >> mark3;
+//
+//	double result = calculate_average_score(mark1, mark2, mark3);
+//
+//	cout << "The average mark is " << result;
+//
+//	return 0;
+//}
+//
+//double calculate_average_score(int a, int b, int c) {
+//	return (a + b + c) / STUDENDS_NUMBER;
+//}
